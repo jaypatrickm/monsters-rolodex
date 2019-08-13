cdd5762476cee5e9f1bd92b4e85ba45bbd15251a
JAYPATRICK MANALANSAN <jaypatrickm@gmail.com>
2019-08-13 01:15:26 -0700

    Updates

---
+++ precache-manifest.9e9723407bca0e3ae226bc3ec97a668d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36caef70ae6da426c0b79e9b4601490",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f2f60f274c70df624ca0",
+    "url": "/monsters-rolodex/static/css/main.9ecd507b.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "f2f60f274c70df624ca0",
+    "url": "/monsters-rolodex/static/js/main.c0761509.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
